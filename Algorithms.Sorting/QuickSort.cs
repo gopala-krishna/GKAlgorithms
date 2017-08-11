f2d6d31755aab7a34e4976247904144b765b0fc4
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-08-11 20:46:41 +0530

    Working Quciksort

--- Algorithms.Sorting/QuickSort.cs
+++ Algorithms.Sorting/QuickSort.cs
@@ -6,67 +6,36 @@ namespace Algorithms.Sorting
 {
     class QuickSort
     {
-        ///// <summary>
-        /////  13 3 4 9  3 13 4 9  3 4 13 9    3 4 9 13
-        /////  
-        /////  
-        ///// 
-        ///// </summary>
-        ///// <param name="InputArr"></param>
-        ///// <param name="arrLength"></param>
-        ///// <returns></returns>
-        //public void QuickSort1(int[] InputArr, int arrLength)
-        //{
-        //    int start = 0; int end = arrLength-1;
+        public void QuickSort1(int[] InputArr, int arrLength)
+        {
+            int start = 0; int end = arrLength - 1;
 
-        //   QuickSortRecursive(InputArr, start, end);
+            SortQuick(InputArr, start, end);
 
-        //    for (int i = 0; i < InputArr.Length; i++)
-        //    {
-        //        Console.Write(InputArr[i] + ",");
-        //    }
-        //}
-
-        //public void QuickSortRecursive(int[] InputArr, int start, int end)
-        //{
-        //    if (start < end)
-        //    {
-        //        int partitionedIndex = Partition(InputArr, start, end);
-        //        if (partitionedIndex > 1)
-        //        {
-        //            QuickSortRecursive(InputArr, start, partitionedIndex - 1);
-        //        }
-        //        if (partitionedIndex + 1 < end)
-        //        {
-        //            QuickSortRecursive(InputArr, partitionedIndex + 1, end);
-        //        }
-        //    }
-
-        //    //return InputArr;
-        //}
-
-        //public int Partition(int[] inputArr, int start, int end)
-        //{
+            for (int i = 0; i < InputArr.Length; i++)
+            {
+                Console.Write(InputArr[i] + ",");
+            }
+        }
 
 
-        //   int pivot = inputArr[end];  int i = start -1; int temp; 
 
-        //    for (int j = start; j < end; j++)
-        //    {
-        //        if( inputArr[j] <= pivot)
-        //        {
-        //            i = i + 1;
-        //            temp = inputArr[i];
-        //            inputArr[i] = inputArr[j];
-        //            inputArr[j] = temp;
-        //        }
-        //     }
-        //    temp = inputArr[i+1];
-        //    inputArr[i+1] = pivot;
-        //    pivot = temp;
 
-        //    return i +1;
-        //}
+        static public void SortQuick(int[] arr, int left, int right)
+        {
+            // For Recusrion  
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                if (pivot > 1)
+                    SortQuick(arr, left, pivot - 1);
+
+                if (pivot + 1 < right)
+                    SortQuick(arr, pivot + 1, right);
+            }
+
+        }
 
 
         static public int Partition(int[] numbers, int left, int right)
@@ -95,35 +64,69 @@ namespace Algorithms.Sorting
 
 
 
-        static public void SortQuick(int[] arr, int left, int right)
-        {
-            // For Recusrion  
-            if (left < right)
-            {
-                int pivot = Partition(arr, left, right);
 
-                if (pivot > 1)
-                    SortQuick(arr, left, pivot - 1);
+        /// <summary>
+        ///  13 3 4 9  3 13 4 9  3 4 13 9    3 4 9 13
+        ///  
+        ///  
+        /// 
+        /// </summary>
+        /// <param name="InputArr"></param>
+        /// <param name="arrLength"></param>
+        /// <returns></returns>
+        //public void QuickSort1(int[] InputArr, int arrLength)
+        //{
+        //    int start = 0; int end = arrLength - 1;
 
-                if (pivot + 1 < right)
-                    SortQuick(arr, pivot + 1, right);
-            }
+        //    QuickSortRecursive(InputArr, start, end);
 
-        }
+        //    for (int i = 0; i < InputArr.Length; i++)
+        //    {
+        //        Console.Write(InputArr[i] + ",");
+        //    }
+        //}
 
-        public void QuickSort1(int[] InputArr, int arrLength)
-        {
-            int start = 0; int end = arrLength - 1;
+        //public static void QuickSortRecursive(int[] InputArr, int start, int end)
+        //{
+        //    if (start < end)
+        //    {
+        //        int partitionedIndex = Partition(InputArr, start, end);
+        //        if (partitionedIndex > 1)
+        //        {
+        //            QuickSortRecursive(InputArr, start, partitionedIndex - 1);
+        //        }
+        //        if (partitionedIndex + 1 < end)
+        //        {
+        //            QuickSortRecursive(InputArr, partitionedIndex + 1, end);
+        //        }
+        //    }
 
-            SortQuick(InputArr, start, end);
+        //    return InputArr;
+        //}
 
-            for (int i = 0; i < InputArr.Length; i++)
-            {
-                Console.Write(InputArr[i] + ",");
-            }
-        }
+        //public static int Partition(int[] inputArr, int start, int end)
+        //{
 
 
+        //    int pivot = inputArr[end]; int i = start - 1; int temp;
 
-        }
+        //    for (int j = start; j < end; j++)
+        //    {
+        //        if (inputArr[j] <= pivot)
+        //        {
+        //            i = i + 1;
+        //            temp = inputArr[i];
+        //            inputArr[i] = inputArr[j];
+        //            inputArr[j] = temp;
+        //        }
+        //    }
+        //    temp = inputArr[i + 1];
+        //    inputArr[i + 1] = pivot;
+        //    pivot = temp;
+
+        //    return i + 1;
+        //}
+
+
+    }
 }
