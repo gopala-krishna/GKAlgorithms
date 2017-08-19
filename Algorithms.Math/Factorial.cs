e7b757c1a3efdcd626dbc4a9f2ba1d3994ac116a
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-08-19 17:17:24 +0530

    .

--- Algorithms.Math/Factorial.cs
+++ Algorithms.Math/Factorial.cs
@@ -36,18 +36,7 @@ namespace Algorithms.Math
         }
 
         /// <summary>
-                //        ///  We know,
-                //log(a* b) = log(a) + log(b)
-
-                //Therefore
-                //log(n! ) = log(1*2*3....... * n)
-                //          = log(1) + log(2) + ........ +log(n)
-
-                //Now, observe that the floor value of log base 
-                //10 increased by 1, of any number, gives the
-                //number of digits present in that number.
-
-                //Hence, output would be : floor(log(n!)) + 1.
+        /// This works for smaller values of n
         /// </summary>
         /// <param name="n"></param>
         public void DigitsCountFactorial1( int n)
@@ -56,6 +45,22 @@ namespace Algorithms.Math
             int digits = factorial.ToString().Length;
             Console.WriteLine(digits);
         }
+
+        /// <summary>
+        //        ///  We know,
+        //log(a* b) = log(a) + log(b)
+
+        //Therefore
+        //log(n! ) = log(1*2*3....... * n)
+        //          = log(1) + log(2) + ........ +log(n)
+
+        //Now, observe that the floor value of log base 
+        //10 increased by 1, of any number, gives the
+        //number of digits present in that number.
+
+        //Hence, output would be : floor(log(n!)) + 1.
+        /// </summary>
+        /// <param name="n"></param>
 
         public void DigitsCountFactorial(int n)
         {
