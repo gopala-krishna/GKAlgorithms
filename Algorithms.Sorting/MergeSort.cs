e708e524e56ce3f7a352716fe339a6d8cef4a02c
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-08-20 18:48:41 +0530

    .

--- Algorithms.Sorting/MergeSort.cs
+++ Algorithms.Sorting/MergeSort.cs
@@ -7,8 +7,8 @@ namespace Algorithms.Sorting
   public class MergeSort
     {
         /// <summary>
-        ///  Time Complexity : O(n log n)
-        ///  Space Complexity : O(n) (merge process : n stack : logn  total = n+logn) so o(n)
+        ///  Time Complexity : O(N log N)
+        ///  Space Complexity : O(n) (merge process : N stack : log N  total = n+logn) so O(N)
         /// </summary>
         /// <param name="input"></param>
         public static void MergeSort1(int[] arr)
@@ -25,7 +25,7 @@ namespace Algorithms.Sorting
         {
             if (low < high)
             {
-                int mid = (low +high )/ 2;
+                int mid = (low + high) / 2;
                 MergeSortRecursive(arr, low, mid);
                 MergeSortRecursive(arr, mid + 1, high);
                 Merge(arr, low, mid, high);
@@ -36,8 +36,8 @@ namespace Algorithms.Sorting
         private static void Merge(int[] arr, int low, int mid, int high)
         {
 
-           int i, j, k;
-            int n1 = mid-low+ 1;
+            int i, j, k;
+            int n1 = mid - low + 1;
             int n2 = high - mid;
 
             /* create temp arrays */
@@ -47,10 +47,12 @@ namespace Algorithms.Sorting
             for (i = 0; i < n1; i++)
                 left[i] = arr[low + i];
             for (j = 0; j < n2; j++)
-                right[j]= arr[mid+ 1 + j];
+                right[j] = arr[mid + 1 + j];
 
-            i = 0;
-            j = 0;
+
+            /* Merge the temp arrays back into arr[l..r]*/
+            i = 0; // Initial index of first subarray
+            j = 0; // Initial index of second subarray
             k = low; // Initial index of merged subarray
             while (i < n1 && j < n2)
             {
@@ -92,53 +94,61 @@ namespace Algorithms.Sorting
 
 
 
-            //int left = low;                                            //4,8,5,1,3,9,2      1 4 5 8   2 3 9     1 2 3 4 5 8 9
-            //int right = middle + 1;
-            //int[] tmp = new int[(high - low) + 1];
-            //int tmpIndex = 0;
+            //    //int left = low;                                            //4,8,5,1,3,9,2      1 4 5 8   2 3 9     1 2 3 4 5 8 9
+            //    //int right = middle + 1;
+            //    //int[] tmp = new int[(high - low) + 1];
+            //    //int tmpIndex = 0;
 
-            //while ((left <= middle) && (right <= high))
-            //{
-            //    if (input[left] < input[right])
-            //    {
-            //        tmp[tmpIndex] = input[left];
-            //        left = left + 1;
-            //    }
-            //    else
-            //    {
-            //        tmp[tmpIndex] = input[right];
-            //        right = right + 1;
-            //    }
-            //    tmpIndex = tmpIndex + 1;
-            //}
+            //    //while ((left <= middle) && (right <= high))
+            //    //{
+            //    //    if (input[left] < input[right])
+            //    //    {
+            //    //        tmp[tmpIndex] = input[left];
+            //    //        left = left + 1;
+            //    //    }
+            //    //    else
+            //    //    {
+            //    //        tmp[tmpIndex] = input[right];
+            //    //        right = right + 1;
+            //    //    }
+            //    //    tmpIndex = tmpIndex + 1;
+            //    //}
 
-            //if (left <= middle)
-            //{
-            //    while (left <= middle)
-            //    {
-            //        tmp[tmpIndex] = input[left];
-            //        left = left + 1;
-            //        tmpIndex = tmpIndex + 1;
-            //    }
-            //}
+            //    //if (left <= middle)
+            //    //{
+            //    //    while (left <= middle)
+            //    //    {
+            //    //        tmp[tmpIndex] = input[left];
+            //    //        left = left + 1;
+            //    //        tmpIndex = tmpIndex + 1;
+            //    //    }
+            //    //}
 
-            //if (right <= high)
-            //{
-            //    while (right <= high)
-            //    {
-            //        tmp[tmpIndex] = input[right];
-            //        right = right + 1;
-            //        tmpIndex = tmpIndex + 1;
-            //    }
-            //}
+            //    //if (right <= high)
+            //    //{
+            //    //    while (right <= high)
+            //    //    {
+            //    //        tmp[tmpIndex] = input[right];
+            //    //        right = right + 1;
+            //    //        tmpIndex = tmpIndex + 1;
+            //    //    }
+            //    //}
 
-            //for (int i = 0; i < tmp.Length; i++)
-            //{
-            //    input[low + i] = tmp[i];
-            //}
+            //    //for (int i = 0; i < tmp.Length; i++)
+            //    //{
+            //    //    input[low + i] = tmp[i];
+            //    //}
+
+            }
+
+
+
+
+
+
+
+
 
         }
 
     }
-        
-}
