2e6bdae401d6367c470da5d653365418b60d9d2b
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-08-21 15:25:19 +0530

    Datastructures added

---
+++ DataStructures.Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DataStructures.Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+
+            ht.Add(1, "Gopala");
+            ht.Add(1, "krishna");
+            ht.Add(4, "Rao");
+
+            foreach (var i in ht)
+            {
+                
+
+
+            }
+
+        }
+    }
+}
