1b9c261568c3a07c0a88fa98c931bed0cbb70b4d
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-08-29 03:05:39 +0530

    .

--- DataStructures.DirectAccessTable/Program.cs
+++ DataStructures.DirectAccessTable/Program.cs
@@ -9,11 +9,14 @@ namespace DataStructures.DirectAccessTable
     class Program
     {
         /// <summary>
-        /// Make a big array and use elements as index in the array. An entry in array is NIL if elements is not present, else the array entry stores pointer to records 
+        /// Make a big array and use elements as index in the array. An entry in array is NIL if elements is not present,
+        /// else the array entry stores pointer to records 
         /// corresponding to element. Time complexity wise this solution is the best among all, we can do all operations in O(1) time. 
-        /// For example to insert an element, we create a record with details of given element, use element as index and store the pointer to the created record in table.
+        /// For example to insert an element, we create a record with details of given element, use element as index and store
+        /// the pointer to the created record in table.
         //This solution has many practical limitations.
-        //First problem with this solution is extra space required is huge.For example if element is n digits, we need O(m* 10n) space for table where m is size of a pointer
+        //First problem with this solution is extra space required is huge.For example if element is n digits, 
+        //we need O(m* 10n) space for table where m is size of a pointer
         //to record.Another problem is an integer in a programming language may not store n digits
         /// </summary>
         /// <param name="args"></param>
