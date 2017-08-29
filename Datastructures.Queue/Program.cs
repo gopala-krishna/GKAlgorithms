72ac24d2ab88cc1b9e0500633497784aa7a8bd0e
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-08-30 00:04:25 +0530

    Added Datastructure projects

---
+++ Datastructures.Queue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructures.Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
