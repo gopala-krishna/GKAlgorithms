6368b80dd93e6b1da3d2c4520066a870a07f5022
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-09-05 06:19:18 +0530

    hashtable basics added

--- DataStructures.HashTable/Program.cs
+++ DataStructures.HashTable/Program.cs
@@ -14,20 +14,24 @@ namespace DataStructures.HashTable
             //string[] arr = new string[] { "bca", "cde", "cac", "cab", "ccc" };
             //ana.AnagramsForWordInDict1("abc", arr);
 
+            HashTableBasics();
+            Console.Read();
+        }
+
+        public static void HashTableBasics()
+        {
             Hashtable ht = new Hashtable();
 
             ht.Add(1, "Gopala");
             //ht.Add(1, "krishna");  // Can't add same key
+            ht.Add(2, "Gopala"); // Can add same value
             ht.Add(4, "Rao");
+
             ht.Add(1.ToString(), "one");
             ht.Add(2.ToString(), "two");
 
-            string value1 = (string)ht[1];
+            string value1 = (string)ht[2];
             string value2 = (string)ht["1"];
-
-            //HashSet<int> hs = new HashSet<int>();
-
-            Console.Read();
         }
     }
 }
