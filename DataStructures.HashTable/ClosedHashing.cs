1841e385ceae8e0d37121ed08a56413db24d561e
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-09-05 18:08:48 +0530

    .

--- DataStructures.HashTable/ClosedHashing.cs
+++ DataStructures.HashTable/ClosedHashing.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,22 @@ namespace DataStructures.HashTable
 {
     /// <summary>
     /// This hashing method is also called "Linear Probing" or "Open Addressing"
+    /// In open addressing, instead of in linked lists, all entry records are stored in the array itself. 
+    /// When a new entry has to be inserted, the hash index of the hashed value is computed and 
+    /// then the array is examined (starting with the hashed index). If the slot at the hashed index 
+    /// is unoccupied, then the entry record is inserted in slot at the hashed index else it proceeds
+    /// in some probe sequence until it finds an unoccupied slot.
     /// </summary>
-    class ClosedHashing
+    public class ClosedHashing
     {
+        public void LinearProbing()
+        {
 
+        }
+
+        public void QuadraticProbing()
+        {
+
+        }
     }
 }
--- DataStructures.HashTable/OpenHashing.cs
+++ DataStructures.HashTable/OpenHashing.cs
@@ -9,8 +9,12 @@ namespace DataStructures.HashTable
 {
     /// <summary>
     /// This hashing method is also called "Seperate Chaining"
+    /// In separate chaining, each element of the hash table is a linked list. 
+    /// To store an element in the hash table you must insert it into a specific
+    /// linked list. If there is any collision (i.e. two different elements have same hash value) 
+    /// then store both the elements in the same linked list.
     /// </summary>
-   public class OpenHashing
+    public class OpenHashing
     {
         /// <summary>
         /// Check whether it is the right approach for chaining
@@ -19,6 +23,7 @@ namespace DataStructures.HashTable
         {
             Hashtable ht = new Hashtable();
 
+            // Insert the elements
             List<string> chain1 = new List<string>();
             chain1.Add("Gopala");
             chain1.Add("Krishna");
@@ -30,6 +35,7 @@ namespace DataStructures.HashTable
             ht.Add(1, chain1);
             ht.Add(2, chain2);
 
+            //Search for the element
             Console.WriteLine(((List<string>)ht[2]).Find(i=>i.Contains("Rao")));
         }
     }
