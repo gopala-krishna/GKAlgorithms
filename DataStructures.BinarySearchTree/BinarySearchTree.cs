3a0b7f71d44e2260bf4a7ef34d07249da4fc3567
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-09-06 19:10:51 +0530

    Binary Search tree complete

---
+++ DataStructures.BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.BinarySearchTree
+{
+   public class BinarySearchTree
+    {
+        /* Class containing left and right child of current node and key value*/
+        public class Node
+        {
+           public int key;
+           public Node left, right;
+
+            public Node(int item)
+            {
+                key = item;
+                left = right = null;
+            }
+        }
+
+        // Root of BST
+        public Node root;
+
+        // Constructor
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        // This method mainly calls insertRec()
+        public void Insert(int key)
+        {
+            root = InsertRec(root, key);
+        }
+
+        /* A recursive function to insert a new key in BST */
+        Node InsertRec(Node root, int key)
+        {
+
+            /* If the tree is empty, return a new node */
+            if (root == null)
+            {
+                root = new Node(key);
+                return root;
+            }
+
+            /* Otherwise, recur down the tree */
+            if (key < root.key)
+                root.left = InsertRec(root.left, key);
+            else if (key > root.key)
+                root.right = InsertRec(root.right, key);
+
+            /* return the (unchanged) node pointer */
+            return root;
+        }
+
+        // This method mainly calls InorderRec()
+        public void Inorder()
+        {
+            InorderRec(root);
+        }
+
+        // A utility function to do inorder traversal of BST
+        void InorderRec(Node root)
+        {
+            if (root != null)
+            {
+                InorderRec(root.left);
+                Console.WriteLine(root.key);
+                InorderRec(root.right);
+            }
+        }
+
+        // This method mainly calls deleteRec()
+        public void DeleteKey(int key)
+        {
+            root = DeleteRec(root, key);
+        }
+
+        /* A recursive function to insert a new key in BST */
+        Node DeleteRec(Node root, int key)
+        {
+            /* Base Case: If the tree is empty */
+            if (root == null) return root;
+
+            /* Otherwise, recur down the tree */
+            if (key < root.key)
+                root.left = DeleteRec(root.left, key);
+            else if (key > root.key)
+                root.right = DeleteRec(root.right, key);
+
+            // if key is same as root's key, then This is the node
+            // to be deleted
+            else
+            {
+                // node with only one child or no child
+                if (root.left == null)
+                    return root.right;
+                else if (root.right == null)
+                    return root.left;
+
+                // node with two children: Get the inorder successor (smallest
+                // in the right subtree)
+                root.key = MinValue(root.right);
+
+                // Delete the inorder successor
+                root.right = DeleteRec(root.right, root.key);
+            }
+            return root;
+        }
+
+        int MinValue(Node root)
+        {
+            int minv = root.key;
+            while (root.left != null)
+            {
+                minv = root.left.key;
+                root = root.left;
+            }
+            return minv;
+        }
+
+
+        public Node Search(Node root, int key)
+        {
+            // Base Cases: root is null or key is present at root
+            if (root == null || root.key == key)
+            {
+                return root;
+            }
+            // val is greater than root's key
+            if (root.key > key)
+                return Search(root.left, key);
+
+            // val is less than root's key
+            return Search(root.right, key);
+        }
+    }
+}
+
---
+++ DataStructures.BinarySearchTree/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree tree = new BinarySearchTree();
+
+            /* Let us create following BST
+                  50
+               /     \
+              30      70
+             /  \    /  \
+           20   40  60   80 */
+
+            tree.Insert(50);
+            tree.Insert(30);
+            tree.Insert(20);
+            tree.Insert(40);
+            tree.Insert(70);
+            tree.Insert(60);
+            tree.Insert(80);
+
+            // print inorder traversal of the BST
+            Console.WriteLine("Inorder traversal of the given tree");
+
+            tree.Inorder();
+
+            Console.WriteLine("\nDelete 20");
+            tree.DeleteKey(20);
+            Console.WriteLine("Inorder traversal of the modified tree");
+            tree.Inorder();
+
+            Console.WriteLine("\nDelete 30");
+            tree.DeleteKey(30);
+            Console.WriteLine("Inorder traversal of the modified tree");
+            tree.Inorder();
+
+            Console.WriteLine("\nDelete 50");
+            tree.DeleteKey(50);
+            Console.WriteLine("Inorder traversal of the modified tree");
+            tree.Inorder();
+
+            //tree.Search(null, 50);
+
+            Console.Read();
+
+        }
+    }
+}
