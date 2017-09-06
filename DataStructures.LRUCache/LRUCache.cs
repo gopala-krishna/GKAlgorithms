0f6f7fbde49eb844ae84da1a000bc38cc09ffd9f
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-09-06 21:32:12 +0530

    LRU cache complete

---
+++ DataStructures.LRUCache/LRUCache.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LRUCache
+{
+    public class Node<D, K>
+    {
+        public D Data { get; private set; }
+        public K Key { get; private set; }
+        public Node<D, K> Previous { get; set; }
+        public Node<D, K> Next { get; set; }
+
+        public Node(D data, K key)
+        {
+            Data = data;
+            Key = key;
+        }
+    }
+    public class LRUCache<K, V>
+    {
+        private readonly int maxCapacity = 0;
+        private readonly Dictionary<K, Node<V, K>> lruCache;
+        private Node<V, K> head = null;
+        private Node<V, K> tail = null;
+
+        public LRUCache(int argMaxCapacity)
+        {
+            maxCapacity = argMaxCapacity;
+            lruCache = new Dictionary<K, Node<V, K>>();
+        }
+
+        public void Insert(K key, V value)
+        {
+            if (lruCache.ContainsKey(key))
+            {
+                MakeMostRecentlyUsed(lruCache[key]);
+            }
+
+            if (lruCache.Count >= maxCapacity)
+            {
+                RemoveLeastRecentlyUsed();
+            }
+
+            Node<V, K> insertedNode = new Node<V, K>(value, key);
+
+            if (head == null)
+            {
+                head = insertedNode;
+                tail = head;
+            }
+            else
+            {
+                MakeMostRecentlyUsed(insertedNode);
+            }
+
+            lruCache.Add(key, insertedNode);
+        }
+
+        private void MakeMostRecentlyUsed(Node<V, K> foundItem)
+        {
+            // Newly inserted item bring to the top
+            if (foundItem.Next == null && foundItem.Previous == null)
+            {
+                foundItem.Next = head;
+                head.Previous = foundItem;
+                if (head.Next == null) tail = head;
+                head = foundItem;
+            }
+            // If it is the tail than bring it to the top
+            else if (foundItem.Next == null && foundItem.Previous != null)
+            {
+                foundItem.Previous.Next = null;
+                tail = foundItem.Previous;
+                foundItem.Next = head;
+                head.Previous = foundItem;
+                head = foundItem;
+            }
+            // If it is an element in between than bring it to the top
+            else if (foundItem.Next != null && foundItem.Previous != null)
+            {
+                foundItem.Previous.Next = foundItem.Next;
+                foundItem.Next.Previous = foundItem.Previous;
+                foundItem.Next = head;
+                head.Previous = foundItem;
+                head = foundItem;
+            }
+            // Last case would be to check if it is a head but if it is than there is no need to bring it to the top
+        }
+
+        private void RemoveLeastRecentlyUsed()
+        {
+            lruCache.Remove(tail.Key);
+            tail.Previous.Next = null;
+            tail = tail.Previous;
+        }
+
+        public Node<V, K> GetItem(K key)
+        {
+            if (!lruCache.ContainsKey(key)) return null;
+
+            MakeMostRecentlyUsed(lruCache[key]);
+
+            return lruCache[key];
+        }
+
+        public int Size()
+        {
+            return lruCache.Count();
+        }
+
+        public string CacheFeed()
+        {
+            var headReference = head;
+
+            List<string> items = new List<string>();
+
+            while (headReference != null)
+            {
+                items.Add(String.Format("[V: {0}]", headReference.Data));
+                headReference = headReference.Next;
+            }
+            return String.Join(",", items);
+        }
+
+       
+    }
+}
--- DataStructures.LRUCache/Program.cs
+++ DataStructures.LRUCache/Program.cs
@@ -10,6 +10,37 @@ namespace DataStructures.LRUCache
     {
         static void Main(string[] args)
         {
+            int cacheSize = 10;
+
+            LRUCache<int, int> lruCache = new LRUCache<int, int>(cacheSize);
+
+            Console.WriteLine(lruCache.Size());
+
+            lruCache.Insert(0, 10);
+            lruCache.Insert(1, 20);
+            lruCache.Insert(2, 30);
+            lruCache.Insert(3, 40);
+            lruCache.Insert(4, 50);
+
+            Console.WriteLine(lruCache.Size());
+            Console.WriteLine(lruCache.CacheFeed());
+
+            lruCache.GetItem(0);
+            lruCache.GetItem(1);
+            lruCache.GetItem(2);
+
+            Console.WriteLine(lruCache.Size());
+            Console.WriteLine(lruCache.CacheFeed());
+
+            lruCache.Insert(5, 60);
+            lruCache.Insert(6, 70);
+            lruCache.Insert(7, 80);
+            lruCache.Insert(8, 90);
+            lruCache.Insert(9, 100);
+
+            Console.WriteLine(lruCache.Size());
+            Console.WriteLine(lruCache.CacheFeed());
+            Console.Read();
         }
     }
 }
