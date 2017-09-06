1288731cf993fc7f48092165a4069ec00587b1c6
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-09-06 20:31:11 +0530

    projects added

---
+++ DataStructures.BinaryTree/BinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.BinaryTree
+{
+    public class Node
+    {
+        public int key;
+        public Node left, right;
+
+        public Node(int item)
+        {
+            key = item;
+            left = right = null;
+        }
+    }
+    public class BinaryTree
+    {
+        // Root of Binary Tree
+            public Node root;
+
+            // Constructors
+            public BinaryTree(int key)
+            {
+                root = new Node(key);
+            }
+
+            public BinaryTree()
+            {
+                root = null;
+            }
+    }
+}
