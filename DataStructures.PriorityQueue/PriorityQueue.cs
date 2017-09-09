cada2ebe3131ab8f44d6360435dc529547c863fe
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-09-10 00:04:56 +0530

    PQ incomplete

---
+++ DataStructures.PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.PriorityQueue
+{
+    class PriorityQueue
+    {
+            int CacheSize = 2;
+            Dictionary<int, KeyValuePair<int, string>> pq = new Dictionary<int, KeyValuePair<int, string>>();
+             static List<KeyValuePair<int, KeyValuePair<int, string>>> orderedPQList;
+
+
+            public void Add(int priority, int key, string value)
+            {
+                pq.Add(priority, new KeyValuePair<int, string>(key, value));
+                var orderedPQ = pq.OrderBy(i => i.Key);
+            
+                if (pq.Count() > CacheSize)
+                {
+                    orderedPQList = orderedPQ.ToList();
+                    pq.Remove(orderedPQList[0].Key);
+                }
+            }
+
+            //public int Get(int key)
+            //{
+            //    foreach (var kv in orderedPQList)
+            //    {
+            //        if (kv.Key.Equals(key))
+            //        {
+            //            pq.Remove(key);
+            //            //pq.Add(kv.Key);
+            //            return kv.Key;
+            //        }
+            //    }
+            //    return default(Key);
+            //}
+
+        public void Print()
+            {
+                foreach (var item in orderedPQList)
+                {
+                    Console.WriteLine("Key :" + item.Key + "  " + "Value :" + item.Value);
+                }
+            }
+     }
+
+    
+}
--- DataStructures.PriorityQueue/Program.cs
+++ DataStructures.PriorityQueue/Program.cs
@@ -10,6 +10,15 @@ namespace DataStructures.PriorityQueue
     {
         static void Main(string[] args)
         {
+            PriorityQueue pq = new PriorityQueue();
+            pq.Add(3,100, "Gpala");
+            pq.Add(8,108, "Gopla");
+            pq.Add(4,104, "Gpala");
+            pq.Add(9,109, "Goala");
+
+            //pq.Print();
+
+            Console.Read();
         }
     }
 }
