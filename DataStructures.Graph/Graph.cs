8291b035985fabfee2efc36bcec1be8cb17107a6
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-09-09 18:30:47 +0530

    Graph added

---
+++ DataStructures.Graph/Graph.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Graph
+{
+    public class Graph
+    {
+        // No. of vertices
+        public int verticesCount;
+        //Adjacency Lists
+        public List<int>[] adjLists;
+
+        // Constructor
+        public Graph(int v)
+        {
+            verticesCount = v;
+            adjLists = new List<int>[v];
+            for (int i = 0; i < v; ++i)
+                adjLists[i] = new List<int>();
+        }
+
+        // Function to add an edge into the graph
+        public void AddEdge(int vertice, int edge)
+        {
+            adjLists[vertice].Add(edge);
+        }
+    }
+}
--- DataStructures.Graph/Program.cs
+++ DataStructures.Graph/Program.cs
@@ -10,6 +10,14 @@ namespace DataStructures.Graph
     {
         static void Main(string[] args)
         {
+            Graph graph = new Graph(4);
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 3);
         }
     }
 }
