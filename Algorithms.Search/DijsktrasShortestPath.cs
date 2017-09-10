8989d89d0045f2189c766542862973c9e0403cd3
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-09-10 22:39:08 +0530

    Dijsktra algo complete

---
+++ Algorithms.Search/DijsktrasShortestPath.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Search
+{
+    class DijsktrasShortestPath
+    {
+        // A utility function to find the vertex with minimum distance value,
+        // from the set of vertices not yet included in shortest path tree
+        static int V = 9;
+        int MinDistance(int[] dist, bool[] sptSet)
+        {
+            // Initialize min value
+            int min = int.MaxValue, min_index = -1;
+
+            for (int v = 0; v < V; v++)
+                if (sptSet[v] == false && dist[v] <= min)
+                {
+                    min = dist[v];
+                    min_index = v;
+                }
+
+            return min_index;
+        }
+
+        // A utility function to print the constructed distance array
+        void PrintSolution(int[] dist, int n)
+        {
+            Console.WriteLine("Vertex   Distance from Source");
+            for (int i = 0; i < V; i++)
+                Console.WriteLine("0" + " -> " +i+" "+ "Shortest distance Is"+" "+ dist[i]);
+        }
+
+        // Funtion that implements Dijkstra's single source shortest path
+        // algorithm for a graph represented using adjacency matrix
+        // representation
+        public void Dijkstra(int[,] graph, int src)
+        {
+            // The output array. dist[i] will hold the shortest distance from src to i
+            int[] dist = new int[V];
+
+            // sptSet[i] will true if vertex i is included in shortest
+            // path tree or shortest distance from src to i is finalized
+            bool[] sptSet = new bool[V];
+
+            // Initialize all distances as INFINITE and stpSet[] as false
+            for (int i = 0; i < V; i++)
+            {
+                dist[i] = int.MaxValue;
+                sptSet[i] = false;
+            }
+
+            // Distance of source vertex from itself is always 0
+            dist[src] = 0;
+
+            // Find shortest path for all vertices
+            for (int count = 0; count < V - 1; count++)
+            {
+                // Pick the minimum distance vertex from the set of vertices
+                // not yet processed. pickedVertexIndex is always equal to src in first
+                // iteration.
+                int pickedVertexIndex = MinDistance(dist, sptSet);
+
+                // Mark the picked vertex as processed
+                sptSet[pickedVertexIndex] = true;
+
+                // Update dist value of the adjacent vertices of the
+                // picked vertex.
+                for (int v = 0; v < V; v++)
+
+                    // Update dist[v] only if is not in sptSet, there is an
+                    // edge from u to v, and total weight of path from src to
+                    // v through u is smaller than current value of dist[v]
+                    if (!sptSet[v] && graph[pickedVertexIndex, v] != 0 && dist[pickedVertexIndex] != int.MaxValue && dist[pickedVertexIndex] + graph[pickedVertexIndex, v] < dist[v])
+                    {
+                        dist[v] = dist[pickedVertexIndex] + graph[pickedVertexIndex, v];
+                    }
+            }
+
+            // print the constructed distance array
+            PrintSolution(dist, V);
+        }
+
+    }
+}
--- Algorithms.Search/Program.cs
+++ Algorithms.Search/Program.cs
@@ -107,6 +107,19 @@ namespace Algorithms.Search
 
 
 
+            int[,] graph = new int[,]{
+                                  {0, 4, 0, 0, 0, 0, 0, 8, 0},
+                                  {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                                  {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                                  {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                                  {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                                  {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                                  {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                                  {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                                  {0, 0, 2, 0, 0, 0, 6, 7, 0}
+                                 };
+            DijsktrasShortestPath dsp = new DijsktrasShortestPath();
+            dsp.Dijkstra(graph, 0);
 
 
 
