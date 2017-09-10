8c5985df22f93e5d956c7f21bd8211531a65ddf8
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-09-10 23:34:43 +0530

    .

--- Algorithms.Search/DijsktrasShortestPathUsingAdjMatrix.cs
+++ Algorithms.Search/DijsktrasShortestPathUsingAdjMatrix.cs
@@ -74,7 +74,10 @@ namespace Algorithms.Search
                     //  2) There exists an edge from pickedVertexIndex to v
                     //  3) Distance is not infinite
                     //  4) Total weight of path from src to v through pickedVertexIndex is smaller than current value of dist[v]
-                    if (sptSet[v] == false && graph[pickedVertexIndex, v] != 0 && dist[pickedVertexIndex] != int.MaxValue && dist[pickedVertexIndex] + graph[pickedVertexIndex, v] < dist[v])
+                    if ( sptSet[v] == false &&
+                        graph[pickedVertexIndex, v] != 0 && 
+                        dist[pickedVertexIndex] != int.MaxValue && 
+                        dist[pickedVertexIndex] + graph[pickedVertexIndex, v] < dist[v])
                     {
                         dist[v] = dist[pickedVertexIndex] + graph[pickedVertexIndex, v];
                     }
