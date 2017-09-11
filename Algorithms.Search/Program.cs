23885bcbb4750752d65565f85cdfd22a73ed0a68
GopalaKrishna <gopalakrishna.iitb@gmail.com>
2017-09-11 09:57:46 +0530

    bellaman ford comp

--- Algorithms.Search/Program.cs
+++ Algorithms.Search/Program.cs
@@ -107,36 +107,71 @@ namespace Algorithms.Search
 
 
 
-            int[,] graph = new int[,]{
-                                  {0, 4, 0, 0, 0, 0, 0, 8, 0},
-                                  {4, 0, 8, 0, 0, 0, 0, 11, 0},
-                                  {0, 8, 0, 7, 0, 4, 0, 0, 2},
-                                  {0, 0, 7, 0, 9, 14, 0, 0, 0},
-                                  {0, 0, 0, 9, 0, 10, 0, 0, 0},
-                                  {0, 0, 4, 14, 10, 0, 2, 0, 0},
-                                  {0, 0, 0, 0, 0, 2, 0, 1, 6},
-                                  {8, 11, 0, 0, 0, 0, 1, 0, 7},
-                                  {0, 0, 2, 0, 0, 0, 6, 7, 0}
-                                 };
-            DijsktrasShortestPathUsingAdjMatrix dsp = new DijsktrasShortestPathUsingAdjMatrix();
-           // dsp.Dijkstra(graph, 3);
-            Console.WriteLine();
-            dsp.Dijkstra(graph, 3, 8,true);
+            // int[,] graph = new int[,]{
+            //                       {0, 4, 0, 0, 0, 0, 0, 8, 0},
+            //                       {4, 0, 8, 0, 0, 0, 0, 11, 0},
+            //                       {0, 8, 0, 7, 0, 4, 0, 0, 2},
+            //                       {0, 0, 7, 0, 9, 14, 0, 0, 0},
+            //                       {0, 0, 0, 9, 0, 10, 0, 0, 0},
+            //                       {0, 0, 4, 14, 10, 0, 2, 0, 0},
+            //                       {0, 0, 0, 0, 0, 2, 0, 1, 6},
+            //                       {8, 11, 0, 0, 0, 0, 1, 0, 7},
+            //                       {0, 0, 2, 0, 0, 0, 6, 7, 0}
+            //                      };
+            // DijsktrasShortestPathUsingAdjMatrix dsp = new DijsktrasShortestPathUsingAdjMatrix();
+            //// dsp.Dijkstra(graph, 3);
+            // Console.WriteLine();
+            // dsp.Dijkstra(graph, 3, 8,true);
 
+            int V = 5;  // Number of vertices in graph
+            int E = 8;  // Number of edges in graph
 
+            Graph1 graph = new Graph1(V, E);
 
+            // add edge 0-1 (or A-B in above figure)
+            graph.edge[0].src = 0;
+            graph.edge[0].dest = 1;
+            graph.edge[0].weight = -1;
 
+            // add edge 0-2 (or A-C in above figure)
+            graph.edge[1].src = 0;
+            graph.edge[1].dest = 2;
+            graph.edge[1].weight = 4;
 
+            // add edge 1-2 (or B-C in above figure)
+            graph.edge[2].src = 1;
+            graph.edge[2].dest = 2;
+            graph.edge[2].weight = 3;
 
+            // add edge 1-3 (or B-D in above figure)
+            graph.edge[3].src = 1;
+            graph.edge[3].dest = 3;
+            graph.edge[3].weight = 2;
 
+            // add edge 1-4 (or A-E in above figure)
+            graph.edge[4].src = 1;
+            graph.edge[4].dest = 4;
+            graph.edge[4].weight = 2;
 
+            // add edge 3-2 (or D-C in above figure)
+            graph.edge[5].src = 3;
 
+            graph.edge[5].dest = 2;
+            graph.edge[5].weight = 5;
 
+            // add edge 3-1 (or D-B in above figure)
+            graph.edge[6].src = 3;
+            graph.edge[6].dest = 1;
+            graph.edge[6].weight = 1;
 
+            // add edge 4-3 (or E-D in above figure)
+            graph.edge[7].src = 4;
+            graph.edge[7].dest = 3;
+            graph.edge[7].weight = -3;
 
-
-
-
+            BellamanFord bf = new BellamanFord();
+            bf.BellamanFord1(graph, 0);
+        
             Console.Read();
 
 
